08f428d2f5039a22992156396951479c655d197f
BlackMProgrammer <cristhiancastro01@gmail.com>
2020-09-09 09:54:15 -0400

    Add project files.

---
+++ DuplicarNumeros/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DuplicarNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite el numero a Duplicar");
+            string numeroval = Console.ReadLine();
+            double numerodpl = Convert.ToDouble(numeroval);
+
+            double duplicado = Math.Pow(numerodpl, 2.00);
+            Console.WriteLine("El numero que eligio fue: {0} y su duplicado es: {1}",numerodpl,duplicado);
+        }
+    }
+}
